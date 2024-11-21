721a4c8569936e52f6a0cb2d0301a3d07ef0de02
egymatador2024 <egymatador2024@gmail.com>
2024-11-21 05:58:24 +0200

    first commit

--- src/app/secondpage/page.tsx
+++ src/app/secondpage/page.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex flex-col justify-center items-center min-h-screen gap-2'>  
-     <h1 className=" text-black text-xl ">
+    <div className='flex flex-col justify-center min-h-screen gap-20 w-[100%] m-auto  '>
+     <h1 style={{borderRadius:'30px 0 30px 0'}} className=" text-black text-xl  bg-orange-200 px-8 py-5 text-center mx-auto rounded-md">
           You need to Login as
      </h1>
-    <Link href="/login" className="bg-orange-400  px-20 py-2 rounded-2xl mb-2 mt-2 text-xl font">
+    <div className='flex flex-col justify-center items-center gap-2'>  
+     
+    <Link href="/login" className="bg-orange-300 text-gray-700  px-20 py-2 rounded-2xl mb-2 mt-2 text-xl font">
     Tourist
    </Link>
-   <Link href="" className="bg-orange-400   px-16 py-2 rounded-2xl text-xl font">
+   <Link href="" className="bg-orange-300 text-gray-700 px-16 py-2 rounded-2xl text-xl font">
     Tour Guide
    </Link>
    </div>
+    </div>
   )
 }
 
