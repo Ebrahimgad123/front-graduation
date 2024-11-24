d7316d0939774354a678789e88c0b4b715479410
egymatador2024 <egymatador2024@gmail.com>
2024-11-24 12:11:08 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // تشغيل الوضع الصارم في React
-  images: {
-    domains: [
-      "lh5.googleusercontent.com",
-      "encrypted-tbn0.gstatic.com",
-      "media.istockphoto.com",
-      "www.egypttoday.com",
-      'www.egypttoursportal.com', 
-      'lh3.googleusercontent.com'
-    ], // السماح بتحميل الصور من هذه النطاقات
-  },
- 
-  output: "standalone",    
+  /* config options here */
 };
 
 export default nextConfig;
