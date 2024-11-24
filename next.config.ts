e3b0c616e5e3964a52f84629aab87b7c8af4e08d
egymatador2024 <egymatador2024@gmail.com>
2024-11-24 16:23:06 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,23 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['*'], 
+    domains: [
+      'lh5.googleusercontent.com', // نطاقات أخرى
+      'lh3.googleusercontent.com', // نطاقات أخرى
+      'encrypted-tbn0.gstatic.com', // أضف هذا النطاق هنا
+      'www.egypttoday.com',
+      'media.istockphoto.com',
+      'https://media.istockphoto.com',
+      'i.natgeofe.com',
+      'www.egypttoursportal.com',
+      'thumbs.dreamstime.com',
+      'beta.sis.gov.eg',
+      'r-xx.bstatic.com',
+      'www.shutterstock.com',
+      'media.gettyimages.com',
+      
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
